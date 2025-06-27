5054bc54461f7e7b578bfd7b3e6173a7721c48f8
sahinton21 <sahinton21@gmail.com>
2025-06-26 20:00:47 -0500

    Added the ability to check vault passwords and delete vaults

--- src/functions/vault.py
+++ src/functions/vault.py
@@ -2,7 +2,10 @@
 import os
 import hashlib
 
-def check_filename(filename):
+BANNED_FILENAME_CHARS = [";", "|", "&"]
+
+
+def check_valid_filename(filename):
   '''
   Make sure the file name input does not have any periods since that is illegal.
   We also have to make sure this file is not already in use
@@ -11,9 +14,65 @@ def check_filename(filename):
 
   # Prevent a semicolon, |, or & since those can be used to mess with file edits
 
-  # If we hit a file then error out
-  return not os.path.isfile(filename)
+  #Also prevent any spaces since those mess with function calls
+  if " " in filename:
+    return False
   
+  for char in BANNED_FILENAME_CHARS:
+    if char in filename:
+      return False
+
+  # This is a good file
+  return True
+
+def check_vault_exists(filename):
+  '''
+  Check if the vault by filename exists
+  '''
+  return os.path.isfile(f"./{filename}.valt")
+
+
+def check_password( vault_name, password_attempt):
+  # read the vault to check if the hashes match and salt match
+  vault_creds = ""
+
+  file = open(f"./{vault_name}.valt", "r") 
+
+  if not file:
+    raise FileNotFoundError(f"Vault {vault_name} does not exist")
+
+
+  # Read out the salt and check if the hashes match
+  
+  
+  vault_creds += file.readline()
+
+  file.close()
+
+
+  salt = vault_creds.split(":")[0]
+  hash_password = vault_creds.split(":")[1].strip()
+
+  # Check the password attempt against the truth
+  hash_attempt = hash_password_and_salt(password_attempt, salt)
+
+  print(hash_attempt)
+  print(hash_password)
+
+  if hash_password == hash_attempt:
+    return True
+  return False
+
+
+
+
+
+def hash_password_and_salt(password, salt):
+  salted_password = password + salt 
+
+  hash = hashlib.sha256(salted_password.encode())
+  return hash.hexdigest()
+
 def generate_salt():
   salt = os.urandom(16)
   salt = salt.hex()
@@ -23,7 +82,7 @@ def generate_salt():
   return salt
 
 
-def create_vault(vault_name, key):
+def create_vault(vault_name, password):
   '''
   Create a new password vault with a SHA256 hash prefix.
   We restrict the following characters 
@@ -36,8 +95,9 @@ def create_vault(vault_name, key):
   '''
   
   # attempt to create a new file. If we fail then return an error message. People could attempt to use this function to do something evil since we are using os commands
-  if not check_filename(vault_name):
-    raise Exception("Invalid character in name or file already in use")
+  if not check_valid_filename(vault_name) or check_vault_exists(vault_name):
+    error_message= f"Invalid character in name or name already in use"
+    raise Exception()
 
   # Create a vault now
   vault = open(vault_name, "w")
@@ -48,12 +108,13 @@ def create_vault(vault_name, key):
   # Generate a salt and hash with the password
   salt = generate_salt()
 
-  salted_key = salt + key
+  #salted_key = salt + key
 
+  
 
-  hash = hashlib.sha256(salted_key.encode())
+  hash = hash_password_and_salt(password, salt)
 
-  vault.write(f"{salt}:{hash.hexdigest()}")
+  vault.write(f"{salt}:{hash}\n")
 
 
   vault.close()
@@ -62,12 +123,21 @@ def create_vault(vault_name, key):
   os.system(f'attrib +h "{vault_name}"')
 
 
-def edit_vault_key(vault_name, key):
+def edit_vault_key(vault_name, password):
   '''
   Swap out the vault key.
   '''
 
-def delete_vault(vault_name, key):
+def delete_vault(vault_name, password):
   '''
   Use the key and confirm the user wants to delete the vault
   '''
+
+  # check if the file exists
+  if  not check_valid_filename(vault_name):
+    error_message= f"No vault with the name {vault_name} was found"
+    raise FileNotFoundError(error_message)
+  
+  # Make sure the password matches what is found in the vault. Delete if the keys match
+  if check_password(vault_name, password):
+    os.remove(f"./{vault_name}.valt")
--- src/main.py
+++ src/main.py
@@ -6,4 +6,10 @@ from functions.vault import *
 if __name__ == "__main__":
   # test some of the functions here 
 
-  create_vault("vault.valt", "password")
+  #create_vault("vault.valt", "password")
+
+  val = check_password("vault", "password")
+
+  delete_vault("vault", "password")
+
+  print(val)
